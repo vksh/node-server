5169971c7f2d21691735cdafc36b1489c34caa0a
Vikash Kumar 8 <vikkumar12@publicisgroupe.net>
2019-02-02 02:25:27 +0530

    change port

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(delay(1000));
 app.use('/users', users);
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 MongoClient.connect(db_url, { useNewUrlParser: true })
     .then(client => {
