b387a61cb335d29a9c6aa475f74e1a2c608e6320
Vikash Kumar 8 <vikkumar12@publicisgroupe.net>
2019-02-02 02:11:46 +0530

    change to run heroku

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(delay(1000));
 app.use('/users', users);
-let port = 3000;
+app.use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+const PORT = process.env.PORT || 5000;
 
 MongoClient.connect(db_url, { useNewUrlParser: true })
     .then(client => {
         const db = client.db('user');
         const collection = db.collection('userDetails');
         app.locals.collection = collection;
-        app.listen(port, () => console.info(`REST API running on port ${port}`));
+        app.listen(PORT, () => console.info(`REST API running on PORT ${PORT}`));
     }).catch(error => console.error(error));
